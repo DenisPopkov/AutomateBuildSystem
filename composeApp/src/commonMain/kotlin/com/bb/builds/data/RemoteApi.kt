84a0659bcb39658ff7ec9244a6a7e1b77e76cda1
DenisPopkov <popkov-den03@mail.ru>
2025-02-08 14:14:18 +0600

    add: test config.json

--- composeApp/src/commonMain/kotlin/com/bb/builds/data/RemoteApi.kt
+++ composeApp/src/commonMain/kotlin/com/bb/builds/data/RemoteApi.kt
@@ -141,6 +141,7 @@ class RemoteApi(
                 val configJson = configResponse.bodyAsText()
                 val config = Json.parseToJsonElement(configJson).jsonObject
                 prodUrl = config["server_url"]?.jsonPrimitive?.content ?: prodUrl
+                println("efefe test - $prodUrl")
             }
         } catch (_: Exception) {}
     }
