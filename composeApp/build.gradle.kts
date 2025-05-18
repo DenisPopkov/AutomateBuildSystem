3c7c30776e63cdfce6e5e693c3498b627ce10673
Denis Popkov <popkov-den03@mail.ru>
2025-05-18 15:05:10 +0600

    fix: client code mac flow

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -76,8 +76,8 @@ android {
         applicationId = "com.bb.builds"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "2.1"
     }
     packaging {
         resources {
