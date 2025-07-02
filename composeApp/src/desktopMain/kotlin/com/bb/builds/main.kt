80d47513241cff6820115f5a432f735231925081
Denis Popkov <popkov-den03@mail.ru>
2025-07-02 13:03:47 +0600

    add: logs

--- composeApp/src/desktopMain/kotlin/com/bb/builds/main.kt
+++ composeApp/src/desktopMain/kotlin/com/bb/builds/main.kt
@@ -6,8 +6,26 @@ import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import com.bb.builds.di.initKoin
 import com.bb.builds.screens.App
+import java.io.File
+import java.io.PrintStream
 
 fun main() = application {
+    Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+        throwable.printStackTrace()
+        try {
+            File("C:/Users/Public/NeuroDesktop/java_crash_full.txt").printWriter().use { out ->
+                out.println("Uncaught exception in thread: ${thread.name}")
+                throwable.printStackTrace(out)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    System.setProperty("skiko.data.path", "C:/Users/Public/NeuroDesktop/AppData/Local")
+    System.setOut(PrintStream(File("C:/Users/Public/NeuroDesktop/java_stdout.txt")))
+    System.setErr(PrintStream(File("C:/Users/Public/NeuroDesktop/java_stderr.txt")))
+
     Window(
         onCloseRequest = ::exitApplication,
         title = "Build Build!",
